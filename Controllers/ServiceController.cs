87e4feca6bde5ae32c34e234f2aa16da2d58fe3e
magnususkali <magnususkali@gmail.com>
2023-03-06 13:22:03 +0100

    ServiceController

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Frisk_2._0.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Login");
+        }
+        public IActionResult Grupp1()
+        {
+            return Redirect("http://193.10.202.75/Frisk2.0/LogIn");
+        }
+        public IActionResult Grupp2()
+        {
+            return Redirect("http://193.10.202.75/Frisk2.0/LogIn");
+        }
+        public IActionResult Grupp3()
+        {
+            return Redirect("http://193.10.202.75/Frisk2.0/LogIn");
+        }
+        public IActionResult Grupp4()
+        {
+            return Redirect("http://193.10.202.75/Frisk2.0/LogIn");
+        }
+        public IActionResult Grupp6()
+        {
+            return Redirect("http://193.10.202.75/Frisk2.0/LogIn");
+        }
+    }
+}
