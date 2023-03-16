0313fc89ada7d3d9347f8a62cb0b6f3040c76e4e
Pontus Lindström <pontus.lindstrom@gmail.com>
2023-03-16 13:57:02 +0100

    Länkar Nav

--- Controllers/ServiceController.cs
+++ Controllers/ServiceController.cs
@@ -14,15 +14,15 @@ namespace Frisk_2._0.Controllers
         }
         public IActionResult Grupp2()
         {
-            return Redirect("http://193.10.202.75/Frisk2.0/LogIn");
+            return Redirect("https://informatik2.ei.hv.se/myevents/Login");
         }
         public IActionResult Grupp3()
         {
-            return Redirect("http://193.10.202.75/Frisk2.0/LogIn");
+            return Redirect("https://informatik3.ei.hv.se/frisk");
         }
         public IActionResult Grupp4()
         {
-            return Redirect("http://193.10.202.75/Frisk2.0/LogIn");
+            return Redirect("https://informatik4.ei.hv.se/SponsorOffer/Login/Index");
         }
         public IActionResult Grupp6()
         {
